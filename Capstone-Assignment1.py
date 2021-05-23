69a4a31f7caccfa68c6821d146fcbf7a7b467345
shahriar-sharifi <48140344+shahriar1990@users.noreply.github.com>
2021-05-24 01:35:45 +0430

    Update and rename .gitignore to Capstone-Assignment1.py

---
+++ Capstone-Assignment1.py
@@ -0,0 +1,4 @@
+# This is a python notebook created for submission of Capstone Project week 1 Assignment. In this notebook I will import pandas as pd and numpy as np.
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
